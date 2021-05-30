5c51519d4f2a3d58c1cbf8148134f467b84609b2
Navjot Garg <nfirebolt@gmail.com>
2021-05-30 14:34:12 -0600

    Imgui premake changes

--- premake5.lua
+++ premake5.lua
@@ -11,12 +11,13 @@ project "ImGui"
     "imconfig.h",
     "imgui.h",
     "imgui.cpp",
-    "imgui_graw.cpp",
+    "imgui_draw.cpp",
     "imgui_internal.h",
     "imgui_widgets.cpp",
+	"imgui_tables.cpp",
     "imstb_rectpack.h",
     "imstb_textedit.h",
-    "imstb_turetype.h",
+    "imstb_truetype.h",
     "imgui_demo.cpp"
   }
 
